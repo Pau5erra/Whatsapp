803ba7acb25e61c5a467d28a9d2a2fa9bf1f6c98
Pau5erra <pausc1@tr12.cat>
2016-11-03 17:38:09 +0100

    primer pantalla i chat
    serveri, nick, port.....

--- _2_1_AdapterViews_ejercicio/src/edu/upc/adapterviews/Activity_chat.java
+++ _2_1_AdapterViews_ejercicio/src/edu/upc/adapterviews/Activity_chat.java
@@ -2,44 +2,59 @@ package edu.upc.adapterviews;
 
 import edu.upc.adapters.MyAdapter_whatsapp;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 /**
  * Created by Pau
  */
 
-public class Activity_chat extends Activity {
+public class Activity_chat extends Activity{
     private ListView conversation;
     private MyAdapter_whatsapp adapter;
     private ArrayList<String> messages;
     private EditText input_text;
+    private TextView title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        Intent intent = new Intent(this, Main.class);
+
         conversation = (ListView) findViewById(R.id.conversation);
         messages = new ArrayList<String>();
         adapter = new MyAdapter_whatsapp(this, messages);
         conversation.setAdapter(adapter);
         input_text = (EditText) findViewById(R.id.input);
+        title = (TextView) findViewById(R.id.title);
 
-        for(int i=0; i<100; i++)
-            messages.add("content of message nº"+i);
+        Bundle extras= getIntent().getExtras();
+        String server = extras.getString("Server");
+        String port = extras.getString("Port");
+        String nick = extras.getString("Nick");
+
+        title.setText(nick + "@" + server + ":" + port);
 
     }
 
     public void addText(final View view) {
 
+        Bundle extras= getIntent().getExtras();
+        String nick = extras.getString("Nick");
+
         if(input_text.getEditableText().toString().equals("")) return;
 
         //...
-        messages.add(input_text.getEditableText().toString());
+        messages.add(nick + " :" +input_text.getEditableText().toString());
         input_text.setText("");
 
         conversation.post(new Runnable() {
--- _2_1_AdapterViews_ejercicio/src/edu/upc/adapterviews/Main.java
+++ _2_1_AdapterViews_ejercicio/src/edu/upc/adapterviews/Main.java
@@ -5,11 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Main extends Activity implements View.OnClickListener {
 
+  private EditText server_label;
+  private String server_string;
+
+  private EditText port_label;
+  private String port_string;
+
+  private EditText nick_label;
+  private String nick_string;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
+
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
@@ -20,10 +31,23 @@ public class Main extends Activity implements View.OnClickListener {
 
     if (arg0 == findViewById(R.id.button_chat)) {
       Intent intent = new Intent(this, Activity_chat.class);
+
+      server_label = (EditText) findViewById(R.id.server);
+      server_string = server_label.getText().toString();
+
+      port_label = (EditText) findViewById(R.id.port);
+      port_string = port_label.getText().toString();
+
+      nick_label = (EditText) findViewById(R.id.nick);
+      nick_string = nick_label.getText().toString();
+
+      intent.putExtra("Server", server_string);
+      intent.putExtra("Port", port_string);
+      intent.putExtra("Nick", nick_string);
+
       startActivity(intent);
     }
-
-
   }
 
 }
+
